a69e51e9ce5f8d40b88afd750eb1a051d1b752e5
yusuf <yusufyi@hotmail.com>
2019-07-16 21:00:00 +0100

    Sent Value to Pages

--- flask1.py
+++ flask1.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def page1():
-   return render_template("page1.html")
+   return render_template("page1.html",no = 1)
 @app.route('/page2/')
 def page2():
-   return render_template("page2.html")
+   return render_template("page2.html",no = 2)
 
 if __name__ == "__main__" :
     app.run(debug=True)
